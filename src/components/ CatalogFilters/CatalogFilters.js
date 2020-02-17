b53ecf4e283fc925a27500d12dc0b0c9d3c12cca
vlad-kiriushchenko <vlad.kiruishchenko.adci@gmail.com>
2020-02-17 12:02:16 +0600

    Fix catalog filter

--- src/components/ CatalogFilters/CatalogFilters.js
+++ src/components/ CatalogFilters/CatalogFilters.js
@@ -10,42 +10,64 @@ import { Checkbox } from '@material-ui/core';
 
 export default function CatalogFilters(props) {
   const defaultList = props.listItems;
-  const arrPrice = props.listItems.map(item => item.props.price)
-  const minValue = Math.min.apply(null, arrPrice)
-  const maxValue = Math.max.apply(null, arrPrice)
+  const arrPrice = valueMinMax(defaultList);
   const [saveStateFilter, setSaveStateFilter] = useState(defaultList);
-  const [value, setValue] = useState([minValue, maxValue]);
+  const [saveStateCheckbox, setSaveStateCheckbox] = useState(defaultList);
+  const [listForCheckbox, setListForCheckbox] = useState(defaultList);
+  const [listForSlider, setListForSlider] = useState(defaultList);
+  const [listForSearch, setListForSearch] = useState(defaultList);
+  const [value, setValue] = useState(arrPrice);
+  const minValue = arrPrice[0];
+  const maxValue = arrPrice[1];
   const [checked, setChecked] = useState('');
+
+  function valueMinMax(arr) {
+    const arrPrice = arr.map(item => item.props.price);
+    return [Math.min.apply(null, arrPrice), Math.max.apply(null, arrPrice)];
+  }
 
   const checkboxChange = (event) => {
     if (event.currentTarget.checked) {
       setChecked(event.currentTarget.value)
-      const clonedListItems = [...defaultList]
+      const clonedListItems = [...listForCheckbox]
       const newListItems = clonedListItems.filter(item => item.props.sex === event.currentTarget.value)
-      props.onChange(newListItems)
-      setSaveStateFilter(newListItems)
+      const listIsEmpty = (newListItems.length === 0)
+      listIsEmpty ? props.onChange('Not found') : props.onChange(newListItems)
+      setValue(valueMinMax(newListItems))
+      setSaveStateCheckbox([...clonedListItems])
+      setListForSlider(newListItems)
+      setListForSearch(newListItems)
     } else {
       setChecked('')
-      props.onChange(saveStateFilter)
-      setSaveStateFilter(saveStateFilter)
+      props.onChange(saveStateCheckbox)
+      setListForCheckbox(saveStateCheckbox)
+      setSaveStateCheckbox(saveStateCheckbox)
+      setListForSlider(saveStateFilter)
+      setValue(valueMinMax(saveStateCheckbox))
+      // setListForSearch(listForSearch)
     }
   };
 
   const handelChange = (e, newValue) => {
-    const clonedListItems = [...saveStateFilter]
+    const clonedListItems = [...listForSlider]
     const newListItems = clonedListItems.filter(item => (newValue[0] <= item.props.price && item.props.price <= newValue[1]))
+    
     props.onChange(newListItems)
-    setSaveStateFilter(newListItems)
+    setSaveStateFilter(clonedListItems)
+    setListForSearch(newListItems)
+    setListForCheckbox(newListItems)
     setValue(newValue)
   }
 
   const searchChange = (e) => {
     const {value} = e.currentTarget
-    const clonedItems = [...saveStateFilter]
+    const clonedItems = [...listForSearch]
     const lowerValue = value.toLowerCase()
     const lowerList = clonedItems.map((item) => ({...item, props: {...item.props, name: item.props.name.toLowerCase()}}))
     const listSearchResults = lowerList.filter(item => item.props.name.indexOf(lowerValue) !== -1)
     const isFind = lowerList.find(item => item.props.name.indexOf(lowerValue) !== -1)
+    // setListForSlider()
+    // setListForCheckbox()
 
     if (isFind) {
       props.onChange(listSearchResults.map((item) => ({...item, props: {...item.props, name: item.props.name[0].toUpperCase() + item.props.name.slice(1)}})))
