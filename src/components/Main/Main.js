14f33e0a18a7312ebd04b78b889c00d952f8c635
v-kiry <vkshazs@gmail.com>
2020-02-29 10:09:12 +0600

    GitHub pages

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Router } from 'react-router-dom'
 import Catalog from "../../pages/Bags/Catalog";
 import Hats from '../../pages/Hats/Hats';
 import Shoes from "../../pages/Shoes";
@@ -9,12 +9,14 @@ import Cart from "../../pages/Cart/Cart";
 export default function Main() {
   return (
     <main>
-      <Switch>
-        <Route exact path='/' component={Catalog}/>
-        <Route path='/shoes' component={Shoes}/>
-        <Route path='/hats' component={Hats}/>
-        <Route path='/cart' component={Cart}/>
-      </Switch>
+      <Router  basename="/web-store-on-react">
+        <Switch>
+          <Route exact path='/' component={Catalog}/>
+          <Route path='/shoes' component={Shoes}/>
+          <Route path='/hats' component={Hats}/>
+          <Route path='/cart' component={Cart}/>
+        </Switch>
+      </Router>
     </main>
   );
 }
